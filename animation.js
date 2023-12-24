3705bf6a89bd4d0b6519ffd46ba08e262d72dfa5
KcasTischaWattt <tmnbob2012@gmail.com>
2023-12-24 16:23:41 +0300

    Put all animation functions in the special file

---
+++ animation.js
@@ -0,0 +1,49 @@
+function restart() {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setValuesToDefault();
+  }
+
+function toggleAnimation() {
+    const animateButton = document.getElementById('animateButton');
+    const buttonText = animateButton.innerText;
+  
+    if (buttonText === 'Animate it') {
+      if (pointsCount >= maxPoints - iterationCount) {
+          restart()
+      }
+      startAnimation();
+    } else {
+      stopAnimation();
+    }
+  }
+  
+  function startAnimation() {
+    const animateButton = document.getElementById('animateButton');
+    animateButton.innerText = 'Stop';
+    animateButton.style.backgroundColor = '#FF1D27';
+  
+    isAnimating = true;
+    animate();
+  }
+  
+  function stopAnimation() {
+    const animateButton = document.getElementById('animateButton');
+    animateButton.innerText = 'Animate it';
+    animateButton.style.backgroundColor = '#007AFF';
+  
+    isAnimating = false;
+    cancelAnimationFrame(animationId);
+  }
+  
+  function animate() {
+    if (pointsCount < maxPoints && isAnimating) {
+      serpinsky(iterationCount);
+      iterationCount *= multiplier;
+      animationId = requestAnimationFrame(animate);
+    } else {
+      stopAnimation();
+      console.log('animation stopped');
+    }
+  }
--- serpinsky.js
+++ serpinsky.js
@@ -12,59 +12,9 @@ function setValuesToDefault() {
   iterationCount = 100;
 }
 
-function restart() {
-  const canvas = document.querySelector('#canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  setValuesToDefault();
-}
-
 function putPoint(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
-}
-
-function toggleAnimation() {
-  const animateButton = document.getElementById('animateButton');
-  const buttonText = animateButton.innerText;
-
-  if (buttonText === 'Animate it') {
-    if (pointsCount >= maxPoints - iterationCount) {
-        restart()
-    }
-    startAnimation();
-  } else {
-    stopAnimation();
-  }
-}
-
-function startAnimation() {
-  const animateButton = document.getElementById('animateButton');
-  animateButton.innerText = 'Stop';
-  animateButton.style.backgroundColor = '#FF1D27';
-
-  isAnimating = true;
-  animate();
-}
-
-function stopAnimation() {
-  const animateButton = document.getElementById('animateButton');
-  animateButton.innerText = 'Animate it';
-  animateButton.style.backgroundColor = '#007AFF';
-
-  isAnimating = false;
-  cancelAnimationFrame(animationId);
-}
-
-function animate() {
-  if (pointsCount < maxPoints && isAnimating) {
-    serpinsky(iterationCount);
-    iterationCount *= multiplier;
-    animationId = requestAnimationFrame(animate);
-  } else {
-    stopAnimation();
-    console.log('animation stopped');
-  }
 }
 
 function serpinsky(iterationCount) {
