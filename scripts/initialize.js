ef0dba04cd93ee3f8f4634b1a502ae35b123f578
KcasTischaWattt <tmnbob2012@gmail.com>
2024-01-01 23:19:35 +0300

    Added initialize.js

---
+++ scripts/initialize.js
@@ -0,0 +1,21 @@
+let pointsCount;
+let iterationCount;
+let animationId;
+let multiplier = 1.001;
+let isAnimating = false;
+let maxPoints = 2500000;
+const scaleFactor = 2 / 3;
+
+function setValuesToDefault() {
+  pointsCount = 0;
+  iterationCount = 100;
+  console.log('Points count: ', 0);
+}
+
+function toggleAccordion() {
+  var accordionBtn = document.querySelector('.accordion');
+  accordionBtn.addEventListener('click', function() {
+      var panel = document.querySelector('.panel');
+      panel.style.display = (panel.style.display === 'block') ? 'none' : 'block';
+  });
+}
--- scripts/serpinsky.js
+++ scripts/serpinsky.js
@@ -1,25 +1,3 @@
-let pointsCount;
-let iterationCount;
-let animationId;
-let multiplier = 1.001;
-let isAnimating = false;
-let maxPoints = 2500000;
-const scaleFactor = 2 / 3;
-
-function setValuesToDefault() {
-  pointsCount = 0;
-  iterationCount = 100;
-  console.log('Points count: ', 0);
-}
-
-function toggleAccordion() {
-  var accordionBtn = document.querySelector('.accordion');
-  accordionBtn.addEventListener('click', function() {
-      var panel = document.querySelector('.panel');
-      panel.style.display = (panel.style.display === 'block') ? 'none' : 'block';
-  });
-}
-
 function putPoint(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
