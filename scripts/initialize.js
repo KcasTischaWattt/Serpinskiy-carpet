75b1bd856cb082501272da90ec2d1fd1220d4fb3
KcasTischaWattt <tmnbob2012@gmail.com>
2024-01-01 23:29:15 +0300

    Changed site initialization

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -7,20 +7,23 @@ let maxPoints = 2500000;
 const scaleFactor = 2 / 3;
 
 function setValuesToDefault() {
-  pointsCount = 0;
-  iterationCount = 100;
-  console.log('Points count: ', 0);
-}
-
-function toggleAccordion() {
-  var accordionBtn = document.querySelector('.accordion');
-  accordionBtn.addEventListener('click', function() {
-      var panel = document.querySelector('.panel');
-      panel.style.display = (panel.style.display === 'block') ? 'none' : 'block';
-  });
-}
-
-function initialize() {
+    pointsCount = 0;
+    iterationCount = 100;
+    console.log('Points count:', pointsCount);
+  }
+  
+  function toggleAccordion() {
+    const accordionBtn = document.querySelector('.accordion');
+    accordionBtn.addEventListener('click', function () {
+      const panel = document.querySelector('.panel');
+      panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
+    });
+  }
+  
+  function initialize() {
     setValuesToDefault();
     toggleAccordion();
-}
+    console.log('Initialized');
+  }
+  
+  document.addEventListener('DOMContentLoaded', initialize);
